2b98a33239c73d58ad6b3777178417d335868b38
HawlyFremont <281623121@qq.com>
2018-07-02 00:14:50 +0800

    checked data before 20180615

--- stock/createSQL.sh
+++ stock/createSQL.sh
@@ -1,5 +1,6 @@
 #use enca or iconv to convert gbk to utf8
 #enca -L zh_CN -x utf-8 history.txt 
 #iconv -f GBK -t UTF-8 h.txt -o h_utf8.txt
+#awk '{print $1,$2,$3,$4,$5,$6,$7,$(NF-1)}' h_utf8.txt  > a.txt
 
-awk '{print "replace into t_trans_history (StockName,Date,Time,Des,Price,Num,Amount,TransID,StockID,CiID,StockholderID ) values (\""$1 "\",\""$2 "\",\""$3"\",\""$4"\",\""$5"\",\""$6"\",\""$7"\",\""$8"\",\""$10"\",\""$9"\",\""$11"\");" }' h_utf8.txt > h.sql
+awk '{print "insert into t_trans_history (StockName,Date,Time,Des,Price,Num,Amount,StockID) values (\""$1 "\",\""$2 "\",\""$3"\",\""$4"\",\""$5"\",\""$6"\",\""$7"\",\""$8"\");" }' a.txt > h.sql
--- stock/createTable.sql
+++ stock/createTable.sql
@@ -1,18 +1,17 @@
 DROP TABLE IF EXISTS `t_trans_history`;
 CREATE TABLE `t_trans_history` (
   `Fid` int(10) unsigned NOT NULL auto_increment,
+  `StockID` int(6) zerofill,
   `StockName` char(16) NOT NULL default '',
-  `Date` int(10) unsigned NOT NULL default '0',
+  `Date` int(10) unsigned NOT NULL default '19700101',
   `Time` time NOT NULL default '00:00:00',
   `Type` tinyint(3) unsigned NOT NULL default '0',
   `Des` char(16) NOT NULL default '',
-  `Price` float NOT NULL default '0',
+  `Price` float NOT NULL default '0.00',
   `Num` int(11) NOT NULL default '0',
-  `Amount` float NOT NULL default '0',
-  `TransID` int(10) unsigned NOT NULL default '0',
-  `StockID` int(10) unsigned NOT NULL default '0',
-  `CiID` int(10) unsigned NOT NULL default '0',
-  `StockholderID` char(16) NOT NULL default '',
-  PRIMARY KEY  (`Fid`),
-  UNIQUE KEY `unikey` (`StockID`,`Date`,`Time`,`Type`,`Num`,`Price`,`CiID`)
+  `vNum` int(11) NOT NULL default '0',
+  `Amount` float NOT NULL default '0.00',  
+  `vAmount` float NOT NULL default '0.00',  
+  PRIMARY KEY  (`Fid`)
+--  UNIQUE KEY `unikey` (`StockID`,`Date`,`Time`,`Type`,`Num`,`Price`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
